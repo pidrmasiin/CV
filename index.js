b2e57cf0aeac7af53f0a0de6289e22e22051bb98
pidrmasiin <petteri.paakkonen@helsinki.fi>
2018-02-12 19:00:26 +0200

    springToReact

---
+++ index.js
@@ -0,0 +1,33 @@
+
+const express = require('express')
+const app = express()
+var morgan = require('morgan')
+
+const bodyParser = require('body-parser')
+
+const cors = require('cors')
+
+const logger = (request, response, next) => {
+  console.log('Method:',request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+
+app.use(bodyParser.json())
+
+app.use(cors())
+
+app.use(logger)
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+
+app.use(express.static('build'))
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
